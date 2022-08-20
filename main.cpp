dc689f7f90aa77eba544fc502c5bd99177e976d9
Zafuzi <zacharyfoutz@gmail.com>
2022-08-20 00:05:19 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+	glfwTerminate();
+	return 0;
+}
